28d17c981346f1bce446164a5e6e620e092d5ec4
Johan Koster <j.koster@alfa-college.nl>
2020-10-26 11:49:26 +0100

    Added designfiles

---
+++ MyComicBookApp/Controllers/ComicBookController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyComicBookApp.Controllers
+{
+    public class ComicBookController : Controller
+    {
+        // GET: ComicBook
+        public ActionResult Detail()
+        {
+            ViewBag.seriesTitle = "The Amazing Spider-Man";
+            ViewBag.issueNumber = 700;
+            ViewBag.description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, " +
+                "last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            //viewbag.blabla = artist();
+            ViewBag.artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+            return View();
+        }
+
+
+    }
+}
